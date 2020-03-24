813435b90079da71f6f441b74ccdee3574cfadce
unknown <you@example.com>
2020-03-24 21:11:20 +0800

    Leap Year Done

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,18 @@ public class LeapYear {
             }
         }
     }
+    
+    /**check if the year is a leapyear*/
+    public static boolean isLeapYear(int year) {
+    	if (year% 400==0) {
+    		return true
+    	}
+    	else if (year%4==0 && year%100 !=0){
+    		return true
+    	}
+    	else {
+    		return false
+    	}		
+    }
 }
 
